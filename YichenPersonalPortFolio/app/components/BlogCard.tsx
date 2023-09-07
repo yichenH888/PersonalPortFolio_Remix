39600a72ab68f660eadecbafb905b26006af38b5
yichenH888 <yh348@duke.edu>
2023-09-07 15:54:41 -0400

    Need to work on integrating link and blog gallery

---
+++ YichenPersonalPortFolio/app/components/BlogCard.tsx
@@ -0,0 +1,23 @@
+export const Blogcard = () => {
+  return (
+    <div>
+      <article
+        className="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl  transition duration-300 ease-in-out"
+        style={{
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80")',
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out" />
+        <div className="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+          <h3 className="text-center">
+            <a className="text-white text-2xl font-bold text-center" href="#">
+              <span className="absolute inset-0" />
+              Top 10 highest paid programming languages of 2021
+            </a>
+          </h3>
+        </div>
+      </article>
+    </div>
+  );
+};
--- YichenPersonalPortFolio/app/routes/posts._index.tsx
+++ YichenPersonalPortFolio/app/routes/posts._index.tsx
@@ -2,7 +2,7 @@ import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getPostListings, getPosts } from "~/models/post.server";
 import type { LoaderFunction } from "@remix-run/node";
-
+import { Blogcard } from "~/components/BlogCard";
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPostListings>>;
 };
@@ -15,6 +15,17 @@ export default function Posts() {
   const { posts } = useLoaderData<typeof loader>() as LoaderData;
   return (
     <main>
+      <h2 className="text-2xl font-extrabold text-gray-900">BLOG</h2>
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mb-12">
+        <article>
+          <section className="mt-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-8">
+            <Blogcard />
+            <Blogcard />
+            <Blogcard />
+            <Blogcard />
+          </section>
+        </article>
+      </section>
       <h1>Posts</h1>
       <Link to="admin" className="text-red-600 underline">
         Admin
@@ -22,7 +33,11 @@ export default function Posts() {
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link to={post.slug} prefetch="intent" className="text-blue-600 underline">
+            <Link
+              to={post.slug}
+              prefetch="intent"
+              className="text-blue-600 underline"
+            >
               {post.title}
             </Link>
           </li>
